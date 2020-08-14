515cd6756efc774512f965e9a169c9809a754dda
Nicholas  Bavafa <nbavafa@NFB-MBP.local>
2020-08-14 02:00:26 -0700

    Updates

---
+++ precache-manifest.67798f44ed5aff696be2bc2ef8300e4c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e509cd966de7cd2a2318fdcd52205a9c",
+    "url": "/PersonalPortfolio/index.html"
+  },
+  {
+    "revision": "ff0024164e7c61a25981",
+    "url": "/PersonalPortfolio/static/css/main.f7de449a.chunk.css"
+  },
+  {
+    "revision": "21d99a99ed99e063be94",
+    "url": "/PersonalPortfolio/static/js/2.5eef0a2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PersonalPortfolio/static/js/2.5eef0a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0024164e7c61a25981",
+    "url": "/PersonalPortfolio/static/js/main.23287910.chunk.js"
+  },
+  {
+    "revision": "d17d7295f12a5796582e",
+    "url": "/PersonalPortfolio/static/js/runtime-main.5e82ec1b.js"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/PersonalPortfolio/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/PersonalPortfolio/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/PersonalPortfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "4d1452520216122f71da13f227bb5a61",
+    "url": "/PersonalPortfolio/static/media/github.4d145252.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/PersonalPortfolio/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "e4832c6c368edee300bf7927447ec8a4",
+    "url": "/PersonalPortfolio/static/media/logo.e4832c6c.png"
+  },
+  {
+    "revision": "fbcb02d21a40e003312efaaf9411b9b9",
+    "url": "/PersonalPortfolio/static/media/logo.fbcb02d2.svg"
+  },
+  {
+    "revision": "bf58874b814ffc108edb45b303a65319",
+    "url": "/PersonalPortfolio/static/media/mail.bf58874b.svg"
+  },
+  {
+    "revision": "7fb70a28d356b2258e39126737ade58c",
+    "url": "/PersonalPortfolio/static/media/me_cropped.7fb70a28.png"
+  },
+  {
+    "revision": "25d1848890ed8348bcaa7b5bb658643c",
+    "url": "/PersonalPortfolio/static/media/resume.25d18488.svg"
+  },
+  {
+    "revision": "6a04bad345d2776cf9144f9e5bed7c73",
+    "url": "/PersonalPortfolio/static/media/resume.6a04bad3.pdf"
+  }
+]);
